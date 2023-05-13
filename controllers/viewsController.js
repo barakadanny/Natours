0d760b4b4046d6e4df67fdaa520ac7b9f0eb2a5b
Danny <barakadan421@gmail.com>
2023-05-13 18:19:31 +0300

    views controller

---
+++ controllers/viewsController.js
@@ -0,0 +1,19 @@
+const Tour = require('../models/tourModel')
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async(req, res, next) => {
+    // * GET /tour
+    const tours  = await Tour.find
+
+    // * Render /tour
+    res.status(200).render('overview', {
+        title: 'All tours',
+        tours
+    })
+})
+
+exports.getTour = catchAsync((req, res, next) => {
+    res.status(200).render('tour', {
+        title: 'tour',
+    })
+})
