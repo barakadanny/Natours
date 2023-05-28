f41de22c148d975115a7f9b42d787e782ff3e725
Danny <barakadan421@gmail.com>
2023-05-29 00:40:47 +0300

    top-5-cheap route added

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getAllTours, createTour, getTour, updateTour, deleteTour} = require('./../controllers/tourController');
+const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours} = require('./../controllers/tourController');
 
 const router = express.Router();
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router
     .route('/')
