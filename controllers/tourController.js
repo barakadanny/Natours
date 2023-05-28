49a82a07fd4ba425adc3b486b614d0279771f6e8
Danny <barakadan421@gmail.com>
2023-05-28 05:19:32 +0300

    deleteTour method implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -79,12 +79,21 @@ const updateTour = async (req, res) => {
 
 }
 
-const deleteTour = (req, res) => {
+const deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id)
+        
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
 }
 
 module.exports = {
