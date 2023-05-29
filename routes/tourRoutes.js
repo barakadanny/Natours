56f547654b8e0b1f25a282e6bad7069804c9bb53
Danny <barakadan421@gmail.com>
2023-05-30 01:44:10 +0300

    getTourStats route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours} = require('./../controllers/tourController');
+const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats} = require('./../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router
     .route('/')
