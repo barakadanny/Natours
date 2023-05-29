0bf20441c3f2ae8ac63fb99087a01411324cb515
Danny <barakadan421@gmail.com>
2023-05-30 02:11:59 +0300

    getMonthlyPlan route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats} = require('./../controllers/tourController');
+const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan} = require('./../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router
     .route('/')
