071e6f82d0110b4ea46f3a8b1c88544055f92d91
Danny <barakadan421@gmail.com>
2023-05-30 01:43:59 +0300

    getTourStats method using pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,7 +14,7 @@ const getAllTours = async (req, res) => {
         // EXECUTE QUERY
         const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
         const tours = await features.query;
-    
+
         // SEND RESPONSE
         res.status(200).json({
             status: 'success',
@@ -94,7 +94,7 @@ const updateTour = async (req, res) => {
 const deleteTour = async (req, res) => {
     try {
         await Tour.findByIdAndDelete(req.params.id)
-        
+
         res.status(204).json({
             status: 'success',
             data: null
@@ -108,11 +108,52 @@ const deleteTour = async (req, res) => {
 
 }
 
+const getTourStats = async (req, res) => {
+    try{
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } }
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    numTours: { $sum: 1 },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' }
+                },
+            },
+            {
+                $sort: { avgPrice: 1 }
+            },
+            {
+                $match: { _id: { $ne: 'EASY' } }
+            }
+        ])
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats
+            }
+        })
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
+}
+
 module.exports = {
     getAllTours,
     getTour,
     createTour,
     updateTour,
     deleteTour,
-    aliasTopTours
+    aliasTopTours,
+    getTourStats
 }
