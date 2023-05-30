8e1d32a0ac70d215eb650804b2bb31a462ddcf54
Danny <barakadan421@gmail.com>
2023-05-31 02:38:22 +0300

    slug for tour name

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true // remove all white spaces in the beginning and end of the string
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -59,6 +61,12 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
 })
+
+// Document middleware: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
