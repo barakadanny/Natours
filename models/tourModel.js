897cdab28733ae7eca9f9871ab1892016743b25a
Danny <barakadan421@gmail.com>
2023-06-01 03:18:51 +0300

    Add Validator to name

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true // remove all white spaces in the beginning and end of the string
+        trim: true, // remove all white spaces in the beginning and end of the string
+        maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+        minlength: [10, 'A tour name must have more or equal than 10 characters']
     },
     slug: String,
     duration: {
