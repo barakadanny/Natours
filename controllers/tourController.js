1b87c8a2db130be8e5b63f5c4ea0c6c0d764fc36
Danny <barakadan421@gmail.com>
2023-06-02 23:53:51 +0300

    404 message to get tour method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync')
+const AppError = require('./../utils/appError')
 
 const aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
@@ -27,6 +28,11 @@ const getAllTours = catchAsync(async (req, res, next) => {
 
 const getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id)
+
+    if(!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -133,9 +139,6 @@ const getMonthlyPlan = catchAsync(async (req, res, next) => {
         {
             $sort: { numTourStarts: -1 }
         }
-        // {
-        //     $limit: 12
-        // }
     ])
 
     res.status(200).json({
