53f8d9ef3cdc940c36437d8b56c07e728e7f88dd
Danny <barakadan421@gmail.com>
2023-06-03 00:15:21 +0300

    Mark error ass operational

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,10 +8,24 @@ const sendErrorDev = (err, res) => {
 }
 
 const sendErrorProd = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    // Operational, trusted error: send message to client
+    // Operational errors are errors that we can predict, such as a user not found error
+    if(err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+    } else {
+        // Programming or other unknown error: don't leak error details
+        // 1) Log error
+        console.error('ERROR', err);
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong'
+        })
+    }
 }
 
 module.exports = (err, req, res, next) => {
