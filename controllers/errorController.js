789ae28b0b77af1c515cb1892391df3a406fdf0e
Danny <barakadan421@gmail.com>
2023-06-03 00:48:35 +0300

    handle invalid id

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+   const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -35,7 +42,10 @@ module.exports = (err, req, res, next) => {
     if(process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+        let error = {...err}; // this will create a hard copy of the err object
+        if(error.name === 'CastError') error = handleCastErrorDB(error);
+
+        sendErrorProd(error, res);
     }
 
 }
