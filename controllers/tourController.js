ee6661e532343cc274ee5fd8b7d588e82d2dd591
Danny <barakadan421@gmail.com>
2023-06-04 15:15:17 +0300

    Update tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,8 +5,8 @@ const AppError = require('./../utils/appError')
 
 const aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
-    req.query.sort = '-ratingsAverage, price';
-    req.query.fields = 'name, price, ratingsAverage, difficulty, summary';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,difficulty,summary';
     next()
 }
 
