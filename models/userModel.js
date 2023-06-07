165020c936f129ab7e7d7fc39504459e27e94182
Danny <barakadan421@gmail.com>
2023-06-07 13:14:52 +0300

    Update routes for login

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,8 @@ const userShema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please provide a password!'],
-        minlength: 8
+        minlength: 8,
+        select: false // this will never show up in any output
     },
     passwordConfirm: {
         type: String,
@@ -36,7 +37,7 @@ userShema.pre('save', async function(next) {
     // Only run this function if password was actually modified (or created) - not when updating user data
     if(!this.isModified('password')) return next();
 
-    // Hash the password with cost of 12 - the higher the cost the longer it takes to hash the password but the more secure it is - 12 is a good number
+    // Hash the password with cost of 12 - the higher the cost the longer it takes to hash the password but the more secure it is
     this.password = await bcrypt.hash(this.password, 12);
 
     // Delete passwordConfirm field - we don't need it anymore - it's only for validation purposes - it's not persisted in the database
@@ -44,6 +45,10 @@ userShema.pre('save', async function(next) {
 
     next();
 });
+
+userShema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userShema);
 
