d7dc802e5753095cd4f0ca2c242dbfe83efeea5f
Danny <barakadan421@gmail.com>
2023-06-07 13:54:37 +0300

    protect get all tour route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan} = require('./../controllers/tourController');
+const { protect } = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router
+    .route('/top-5-cheap')
+    .get(aliasTopTours, getAllTours);
 
-router.route('/tour-stats').get(getTourStats);
+router
+    .route('/tour-stats')
+    .get(getTourStats);
 
-router.route('/monthly-plan/:year').get(getMonthlyPlan);
+router
+    .route('/monthly-plan/:year')
+    .get(getMonthlyPlan);
 
 router
     .route('/')
-    .get(getAllTours)
+    .get(protect, getAllTours)
     .post(createTour)
 
 router
