0522fad42f98d57df9d57be9ccc1d27b5d8d97d0
Danny <barakadan421@gmail.com>
2023-06-09 00:32:14 +0300

    Add roles to model

--- models/userModel.js
+++ models/userModel.js
@@ -12,14 +12,19 @@ const userShema = new mongoose.Schema({
         required: [true, 'Please provide your email!'],
         unique: true,
         lowercase: true,
-        validate: [validator.isEmail, 'Please provide a valid email!'] // validator package
+        validate: [validator.isEmail, 'Please provide a valid email!']
     },
     photo: String,
+    role: {
+        type: String,
+        enum: ['user', 'guide', 'lead-guide', 'admin'],
+        default: 'user'
+    },
     password: {
         type: String,
         required: [true, 'Please provide a password!'],
         minlength: 8,
-        select: false // this will never show up in any output
+        select: false // password will never show up in any output
     },
     passwordConfirm: {
         type: String,
