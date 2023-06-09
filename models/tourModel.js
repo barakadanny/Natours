c4439bd0e8ba746c51c2ba105c90e311d7dbcb43
Danny <barakadan421@gmail.com>
2023-06-09 21:08:06 +0300

    Use reference user to tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel')
-// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -104,7 +102,6 @@ const tourSchema = new mongoose.Schema(
       }
     ],
     guides: [
-      // Embedding
       {
         type: mongoose.Schema.ObjectId,
         ref: 'User'
@@ -124,18 +121,6 @@ tourSchema.virtual('durationWeeks').get(function() {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-// this code does the following: 
-// create a new array of promises, 
-// each promise is a query to the User 
-// model to find the user with the id in the guides array, 
-// then we await all the promises to resolve and then we assign 
-// the result to the guides array
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
