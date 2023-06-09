7374f3fbabb3ad627c2c9b335e46a881ef8dc88b
Danny <barakadan421@gmail.com>
2023-06-09 20:55:15 +0300

    Embed tour for guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel')
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -102,6 +103,13 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
+    guides: [
+      // Embedding
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -116,6 +124,18 @@ tourSchema.virtual('durationWeeks').get(function() {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// this code does the following: 
+// create a new array of promises, 
+// each promise is a query to the User 
+// model to find the user with the id in the guides array, 
+// then we await all the promises to resolve and then we assign 
+// the result to the guides array
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
