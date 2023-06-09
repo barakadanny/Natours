a5888f8611e661a02a8cce151342613fa87582ce
Danny <barakadan421@gmail.com>
2023-06-09 23:26:34 +0300

    Filter review per tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,7 +3,11 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-    const reviews = await Review.find();
+    let filter = {};
+    if(req.params.tourId) filter = { tour: req.params.tourId };
+
+    const reviews = await Review.find(filter);
+
     res.status(200).json({
         status: 'success',
         results: reviews.length,
@@ -19,7 +23,7 @@ exports.createReview = catchAsync(async (req, res, next) => {
     if(!req.body.user) req.body.user = req.user.id;
 
     const newReview = await Review.create(req.body);
-    
+
     res.status(201).json({
         status: 'success',
         data: {
