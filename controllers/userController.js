472b0ef90b0754f807554fe63d132451e89f10e4
Danny <barakadan421@gmail.com>
2023-06-10 01:23:21 +0300

    Update Message

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,7 +67,7 @@ exports.getUser = factory.getOne(User);
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'Please use /signup instead'
   });
 };
 
