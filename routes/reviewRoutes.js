1a7fcce75272eb24622df1e00a55f74d5db9f39e
Danny <barakadan421@gmail.com>
2023-06-10 01:10:33 +0300

    Add setTourUserIds middleware to review routes post

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllReviews, createReview, deleteReview, updateReview } = require('./../controllers/reviewController');
+const { getAllReviews, createReview, deleteReview, updateReview, setTourUserIds } = require('./../controllers/reviewController');
 const { protect, restrictTo } = require('./../controllers/authController');
 
 // mergeParams: true allows us to access the tourId from the tour router
@@ -8,7 +8,7 @@ const router = express.Router({ mergeParams: true });
 router
     .route('/')
     .get(getAllReviews)
-    .post(protect, restrictTo('user'), createReview)
+    .post(protect, restrictTo('user'),setTourUserIds , createReview)
 
 router
     .route('/:id')
