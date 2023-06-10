0d786230d5dbb247fd7d73071f1fe43e26ce854a
Danny <barakadan421@gmail.com>
2023-06-10 19:57:55 +0300

    startLocation index added

--- models/tourModel.js
+++ models/tourModel.js
@@ -116,6 +116,7 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' }); // 2dsphere - for geospatial data
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
