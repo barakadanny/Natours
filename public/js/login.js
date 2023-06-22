a2064cee40a4fc49c8e52d7d7a4973c47d8db583
Danny <barakadan421@gmail.com>
2023-06-23 02:28:30 +0300

    Logout function

--- public/js/login.js
+++ public/js/login.js
@@ -21,3 +21,21 @@ export const login = async (email, password) => {
         showAlert('error', err.response.data.message);
     }
 }
+
+export const logout = async () => {
+    try{
+        const res = await axios({
+            method: 'GET',
+            url: 'http://localhost:3000/api/v1/users/logout'
+        });
+        if(res.data.status === "success") {
+            // location.reload(true);
+            showAlert('success', 'Logged Out!')
+            window.setTimeout(() => {
+                location.assign('/')
+            }, 800)
+        }
+    } catch(err) {
+        showAlert('error', 'Error while logging out! Please try again!');
+    }
+}
