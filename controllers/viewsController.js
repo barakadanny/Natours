9ad292b17a05c92359092be913c7b447c87a98b8
Danny <barakadan421@gmail.com>
2023-06-23 02:35:53 +0300

    Handle tour not found

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel')
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getHomeTours = catchAsync(async(req, res, next)=>{
     res.status(200).render('home', {
@@ -24,11 +25,11 @@ exports.getTour = catchAsync(async(req, res, next) => {
         fields: 'review rating user'
     });
 
+    if(!tour) {
+        return next(new AppError('There is no tour with that name.', 404))
+    }
+
     res.status(200)
-        // .set(
-        //     'Content-Security-Policy',
-        //     'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com'
-        // )
         .render('tour', {
             title: `${tour.name} Tour`,
             tour
@@ -38,10 +39,6 @@ exports.getTour = catchAsync(async(req, res, next) => {
 exports.getLoginForm = catchAsync(async(req, res, next) => {
     res
         .status(200)
-        // .set(
-        //     'Content-Security-Policy',
-        //     "connect-src 'self' https://cdnjs.cloudflare.com"
-        // )
         .render('login', {
             title: 'Login'
         })
