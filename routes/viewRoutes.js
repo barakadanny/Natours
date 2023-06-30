f5a57d465be6ed1a2c64435b0eb5eff677c88f5c
Danny <barakadan421@gmail.com>
2023-06-30 18:36:27 +0300

    signup route added

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const { getHomeTours, getOverview, getTour, getLoginForm, getAccount } = require('../controllers/viewsController');
+const { 
+        getHomeTours, 
+        getOverview, 
+        getTour, 
+        getLoginForm, 
+        getAccount, 
+        getSignUpForm 
+    } = require('../controllers/viewsController');
+
 const { isLoggedIn, protect } = require('../controllers/authController')
 
 const router = express.Router();
@@ -13,5 +21,7 @@ router.get('/tour/:slug', isLoggedIn, getTour)
 router.get('/login', isLoggedIn, getLoginForm)
 
 router.get('/me', protect, getAccount)
+
+router.get('/signup', getSignUpForm)
 
 module.exports = router
