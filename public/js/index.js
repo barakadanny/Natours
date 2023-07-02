add01ad82ba38cf01a4c74942c36b7d1097d5b88
Danny <barakadan421@gmail.com>
2023-07-02 21:56:51 +0300

    Update form to add user photo

--- public/js/index.js
+++ public/js/index.js
@@ -33,10 +33,12 @@ if (signupForm)
 if (userDataForm)
     userDataForm.addEventListener('submit', e => {
         e.preventDefault();
-        const name = document.querySelector('#name').value;
-        const email = document.querySelector('#email').value;
+        const form = new FormData();
+        form.append('name', document.querySelector('#name').value);
+        form.append('email', document.querySelector('#email').value);
+        form.append('photo', document.querySelector('#photo').files[0]);
 
-        updateSettings({name, email}, 'data');
+        updateSettings(form, 'data');
     });
 
 if (userPasswordForm)
